cc8df9b20cc3dbf8bcf05e12e529d24a355869ea
João Pedro Chagas <jpchagas@gmail.com>
2021-03-27 10:36:53 -0300

    begin project

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const config = require('config')
+
+const app = express()
+
+
+app.use(bodyParser.json())
+
+app.listen(config.get('api.porta'), ()=>{
+    console.log('Service Working')
+})
